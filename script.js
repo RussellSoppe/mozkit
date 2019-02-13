e621e25e37dda7557a8e42bf3019513fb3a2440d
russellicious <42648750+russellicious@users.noreply.github.com>
2019-02-13 09:57:22 -0600

    Added a second container

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let container = document.getElementById("container");
+let container2 = document.getElementById("container2");
 let front = document.getElementsByClassName("front");
 let back = document.getElementsByClassName("back");
 
-const flipMe = ()=>{
-	container.className = "flipper";
+const flipMe = (id)=>{
+	id.className = "flipper";
 	const flipBack = ()=>{
-		container.className = "flipback";
+		id.className = "flipback";
 	};
 	setTimeout(flipBack, 3000);
 };
 
-container.addEventListener("click", flipMe);
+container.addEventListener("click", ()=>flipMe(container));
+container2.addEventListener("click", ()=>flipMe(container2));
